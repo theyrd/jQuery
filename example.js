131036474e79716b5e4e3cdaebcf9383848d02a6
theyrd <84168351+theyrd@users.noreply.github.com>
2022-01-02 07:48:34 +0200

    Update example.js

--- example.js
+++ example.js
@@ -4,6 +4,20 @@
         // checking if inside jQuery
         console.log('Ready to write some jQuery code.');
 
+        // jQuery version
+        if (typeof jQuery !== 'undefined') {  
+           console.log("jQuery version: ", jQuery.fn.jquery);
+        }
+
+        // Remove a specific value from an array using jQuery // or get a specific value
+        let y = ['Red', 'Green', 'White', 'black', 'Yellow'];
+        let remove_item = 'White';
+        console.log('Array before removing the element = '+y);
+        y = $.grep(y, function(value){
+            return value != remove_item;
+        });
+        console.log('Array after removing the element = '+y);
+
         // disable right click on page or change the menu
         $(document).bind("contextmenu", function(){
             return false;
@@ -30,20 +44,35 @@
 
         // disable form submit button if checkbox is not checked
         $("<div class='div-5'></div").appendTo("body");
-        $("<form class='form-container'></form>").appendTo(".div-5");
-        $("<input type='text' placeholder='Enter your name' class='input'/>").appendTo(".form-container");
-        $("<input type='text' placeholder='Enter your last name' class='input'/>").appendTo(".form-container");
+        $("<form action='' class='form-container'></form>").appendTo(".div-5");
+        $("<input type='text' placeholder='Enter your name' class='input-form'/>").appendTo(".form-container");
+        $("<input type='text' placeholder='Enter your last name' class='input-form'/>").appendTo(".form-container");
         $("<b>Accept page terms</b><input type='checkbox' class='checkbox'/>").appendTo(".form-container");
-        $("<input id='form-btn' class='btn' disabled='disabled' type='submit' style='background: #ccc;'/>").appendTo(".form-container");
+        $("<input type='submit' id='form-btn' class='btn' value='Accept terms' disabled='disabled' style='background: #ccc;'/>").appendTo(".form-container");
         $(".checkbox").click(function(){
             if($("#form-btn").is(":disabled")){
                 $("#form-btn").removeAttr("disabled");
                 $("#form-btn").css("background", "#06d6a0");
+                // $("#form-btn").prop("value", "Submit"); // change innerHTML value // prop
+                $("#form-btn").val("Submit"); // or by just using val()
             }
             else {
                 $("#form-btn").attr("disabled", "disabled");
                 $("#form-btn").css("background", "#ccc");
+                // $("#form-btn").prop("value", "Accept terms"); // change innerHTML value back to defualt // prop
+                $("#form-btn").val("Accept terms"); // using val() insted of prop 
             }
+        });
+        // if enter key pressed or click was made out of the input box
+        $(".input-form").change(function() {
+            $(this).css("background-color", "#06d6a0");
+        });
+
+        // get all the forms elements and their values
+        $('#myForm').submit(function(e) {
+            var values = $(this).serialize();
+            console.log(values);
+            e.preventDefault();
         });
 
         // hide broken image (not the right link or link dosen't work) - change img src to check
@@ -97,7 +126,7 @@
             $("<p> bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>").appendTo(".div-4");
         }
 
-        // 
+        // max length in the input box is 15, make a count down number starting at 15
         $("<div class='div-max15'></div").appendTo("body");
         $("<form class='form-container-2'></form>").appendTo(".div-max15");
         $("<label>Maximum 15 characters</label>").appendTo(".form-container-2");
@@ -157,15 +186,134 @@
             $("#header-btn").hide()
         });
 
-        function callNumber(){
-            var number = 5;
-            var number = 6;
-            // let num = 5;
-            // let num = 10;
-            // let num = 12;
-            console.log(number, num);
+        // delete all options when button was clicked
+        $("#myColorBtn").click(function(){
+            $("#myColor")
+            .empty()
+            .append("<option selected='selected' value='White'>White</option>");
+        });
+
+        // change text style with change()
+        $("#textStyle-0, #textStyle-1, #textStyle-2").change(function(){
+            if ($(this).val() == "italic"){
+                if ($(this).is(':checked')){
+                    $("#styledText").css("font-style", "italic");
+                }
+                else {
+                    $("#styledText").css("font-style", "normal");
+                }
+            }
+            else if ($(this).val() == "bold"){
+                if ($(this).is(':checked')){
+                    $("#styledText").css("font-weight", "bold");
+                }
+                else {
+                    $("#styledText").css("font-weight", "normal");
+                }
+            }
+            else if ($(this).val() == "underline"){
+                if ($(this).is(':checked')){
+                    $("#styledText").css("text-decoration", "underline");
+                }
+                else {
+                    $("#styledText").css("text-decoration", "none");
+                }
+            }
+        });
+
+        // change text style with change() option 2 and shorter
+        $('#textStyle-0, #textStyle-1, #textStyle-2').click(function () {
+            let checked = $(this).is(':checked');
+            let value = $(this).attr('value');
+            if(checked) {
+                $('#styledText').addClass(value);
+            } else {
+                $('#styledText').removeClass(value);
+            }
+        });
+        // add a css class/id with addClass() : $('#styledText').addClass(value);
+        // remove a css class/id with removeClass() : $('#styledText').removeClass(value);
+
+        // get the word value typed in the input field in the moment // (search box)
+        $("#styledText").keyup(function(){
+            let tvalue = $(this).val();
+            console.log(tvalue);
+        }).keyup();
+
+        // get input field value on enter key press
+        $("#styledText").keypress(function(e){
+            if (e.which == 13){
+                let text = $("#styledText").val();
+                console.log(text);
+                e.preventDefault(); // on enter don't go up or next
+                // redirect to another page using simple JS
+                // window.location.replace("https://stackoverflow.com");
+                // redirect to another page using jQuery
+                // $(location).prop('href', 'https://stackoverflow.com')
+            }
+        });
+
+        // Convert a jQuery object into a string
+        let element = $('<h1>',{
+            text : "Convert a jQuery object into a string.",
+            class : "test"
+        });        
+        // $('#output').text( element.get(0).outerHTML);
+        $('#output').html( element.get(0).outerHTML);
+
+        // append to a empty select html tag values
+        let myOptions = {
+            val1: 'Blue',
+            val2: 'Red',
+            val3: 'Yellow',
+            val4: 'Purple',
+            val5: 'Black',
         }
-        
+        let mySelect = $("#some-color-option");
+        $.each(myOptions, function(val, text){
+            mySelect.append($("<option></option>").val(val).html(text));
+        });
+        // change color as the given option
+        if ($("#some-color-option").val() == "val1"){
+            $("#some-color-option").css("color", "blue");
+        }
+        $("#some-color-option").click(function(){
+            if ($("#some-color-option").val() == "val1"){
+                $("#some-color-option").css("color", "blue");
+            }
+            else if ($("#some-color-option").val() == "val2"){
+                $("#some-color-option").css("color", "red");
+            }
+            else if ($("#some-color-option").val() == "val3"){
+                $("#some-color-option").css("color", "yellow");
+            }
+            else if ($("#some-color-option").val() == "val4"){
+                $("#some-color-option").css("color", "purple");
+            }
+            else if ($("#some-color-option").val() == "val5"){
+                $("#some-color-option").css("color", "black");
+            }
+        });
+
+        // Count child elements
+        const count = $("#some-color-option option").length;
+        console.log(count);
+
+        // only number input feild logic
+        $('.numbers').keyup(function () { 
+            this.value = this.value.replace(/[^0-9\.]/g,'');
+        });
+
+        // Set a value in a span
+        $("#spanid").html(" Exercises Solution");
+
+        // change html text and url with attribute name
+        $("#a-1").html("w3resource");
+        $("#a-1").attr("href", "https://www.w3resource.com/");
+
+        // Find the total width of an element (including width, padding, and border)
+        $("<p id='myText'>jQuery Exercises, Practice, Solution</p>").appendTo("#output");
+        console.log("Width + padding + borders : " + $("#myText").outerWidth());
 
     });
 }) (jQuery);
